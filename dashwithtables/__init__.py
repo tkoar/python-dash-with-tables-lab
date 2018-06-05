87bffde02e642637b498f4b1a8a868aa462b0ea1
tkoar <terrancekoar@gmail.com>
2018-06-04 22:30:12 -0400

    removed solution

--- dashwithtables/__init__.py
+++ dashwithtables/__init__.py
@@ -1,53 +1,11 @@
-#import dash
-import dash
-#import dash core components as dcc
-import dash_core_components as dcc
-#import dash html components as html
-import dash_html_components as html
+# import dash
+
+# import dash core components as dcc
+
+# import dash html components as html
+
 # import Input, Output from dash.dependencies for callback functions
-from dash.dependencies import Input, Output
 
 # import data about interest of pho/ramen/soba
-from dashwithtables.food_interest_data import data
 
 # create dash app and make path '/'
-app = dash.Dash(__name__, url_base_pathname='/')
-
-def generate_table(table_data=data):
-    return html.Table(id='food-table', children=
-        # create table headers
-        [html.Tr(id='headers', children=[html.Th(col) for col in table_data[0].keys()])]
-        + # combine both table headers and cells into one list
-        # create table cells
-        [html.Tr(id='row-data', children=[
-            html.Td(data_dict[column]) for column in data_dict.keys()
-        ]) for data_dict in table_data]
-    )
-
-def generate_drop_down():
-    return dcc.Dropdown(
-        id='sort-by-selector',
-        options=[
-            {'label': 'Country', 'value': 'Country'},
-            {'label': 'Pho', 'value': 'Pho'},
-            {'label': 'Ramen', 'value': 'Ramen'},
-            {'label': 'Soba', 'value': 'Soba'}
-        ],
-        value="Country"
-    )
-
-
-app.layout = html.Div(children=[
-    generate_drop_down(),
-    html.H3(children='Interest in Pho, Ramen, and Soba by Country according to Google Search from 01/2004 - 06/2018'),
-    html.Div(id='table_container')
-])
-
-@app.callback(
-    Output(component_id='table_container', component_property='children'),
-    [Input(component_id='sort-by-selector', component_property='value')]
-)
-def sort_table(input_value):
-    global data
-    sorted_data = sorted(data, key=lambda datum: datum[input_value])
-    return generate_table(sorted_data)
